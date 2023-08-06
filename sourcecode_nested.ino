0e652ef2f14a1ef36b2c9409e14a5bb7af56609f
Ayush karwa <89857864+AYUSH737565@users.noreply.github.com>
2023-08-06 14:48:15 +0530

    Update sourcecode_nested.ino
    added distance calculating formula which was missing.

--- sourcecode_nested.ino
+++ sourcecode_nested.ino
@@ -5,49 +5,99 @@ const int frontechoPin = 5;
 const int righttrigPin = 6;
 const int rightechoPin = 7;
 
-const leftmotorPinA = 9;
-const leftmotorPinB = 10;
-const rightmotorPinA = 11;
-const rightmotorPinB = 12;
+const int leftmotorPinA = 9;
+const int leftmotorPinB = 10;
+const int rightmotorPinA = 11;
+const int rightmotorPinB = 12;
 
-const int thresholdDistance = 20; //20 centimeters
+const int thresholdDistance = 20; // 20 centimeters
 
-
-
-
-
-void setup(){
-  pinMode(lefttrigPin,OUTPUT);
-  pinMode(leftechoPin,INPUT);
-  pinMode(fronttrigPin,OUTPUT);
-  pinMode(frontechoPin,INPUT);
-  pinMode(righttrigPin,OUTPUT);
-  pinMode(rightechoPin,INPUT);
-  pinMode(leftmotorPinA,OUTPUT);
-  pinMode(leftmotorPinB,OUTPUT);
-  pinMode(rightmotorPinA,OUTPUT);
-  pinMode(rightmotorPinB,OUTPUT);
-
-  Serial.print(9600);
-
-  stopMotors();
-  
+void stopMotors() {
+  digitalWrite(leftmotorPinA, LOW);
+  digitalWrite(leftmotorPinB, LOW);
+  digitalWrite(rightmotorPinA, LOW);
+  digitalWrite(rightmotorPinB, LOW);
 }
 
-void loop(){
+void moveForward() {
+  digitalWrite(leftmotorPinA, HIGH);
+  digitalWrite(leftmotorPinB, LOW);
+  digitalWrite(rightmotorPinA, HIGH);
+  digitalWrite(rightmotorPinB, LOW);
+}
 
-  int leftdistance = getDistance(lefttrigPin,leftechoPin);
-  int frontdistance = getDistance(fronttrigPin,frontechoPin);
-  int rightdistance = getDistance(righttrigPin.rightechoPin);
+void moveLeft() {
+  digitalWrite(leftmotorPinA, LOW);
+  digitalWrite(leftmotorPinB, HIGH);
+  digitalWrite(rightmotorPinA, HIGH);
+  digitalWrite(rightmotorPinB, LOW);
+  delay(500);
+  stopMotors();
+}
 
+void moveRight() {
+  digitalWrite(leftmotorPinA, HIGH);
+  digitalWrite(leftmotorPinB, LOW);
+  digitalWrite(rightmotorPinA, LOW);
+  digitalWrite(rightmotorPinB, HIGH);
+  delay(500);
+  stopMotors();
+}
+
+void turn180Degree() {
+  digitalWrite(leftmotorPinA, HIGH);
+  digitalWrite(leftmotorPinB, LOW);
+  digitalWrite(rightmotorPinA, LOW);
+  digitalWrite(rightmotorPinB, HIGH);
+  delay(2000);
+  stopMotors();
+}
+
+long getDistance(int trigPin, int echoPin) {
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  
+  long duration = pulseIn(echoPin, HIGH);
+  long distance = duration * 0.034 / 2;  // distance in centimeters
+  
+  return distance;
+}
+
+void setup() {
+  pinMode(lefttrigPin, OUTPUT);
+  pinMode(leftechoPin, INPUT);
+  pinMode(fronttrigPin, OUTPUT);
+  pinMode(frontechoPin, INPUT);
+  pinMode(righttrigPin, OUTPUT);
+  pinMode(rightechoPin, INPUT);
+  pinMode(leftmotorPinA, OUTPUT);
+  pinMode(leftmotorPinB, OUTPUT);
+  pinMode(rightmotorPinA, OUTPUT);
+  pinMode(rightmotorPinB, OUTPUT);
+
+  Serial.begin(9600);  // to print distance
+  
+  stopMotors();
+}
+
+void loop() {
+  int leftdistance = getDistance(lefttrigPin, leftechoPin);
+  int frontdistance = getDistance(fronttrigPin, frontechoPin);
+  int rightdistance = getDistance(righttrigPin, rightechoPin);  
+  
   Serial.print("leftdistance:");
   Serial.println(leftdistance);
   Serial.print("frontdistance:");
   Serial.println(frontdistance);
-  Serial.print("rightdistance");
+  Serial.print("rightdistance:");
   Serial.println(rightdistance);
 
-  if(frontdstance <= thresholdDistance){
+ 
+ if(frontdistance <= thresholdDistance){
   
   if(rightdistance <= thresholdDistance && leftdistance <= thresholdDistance){
   stopMotors();
@@ -68,10 +118,11 @@ void loop(){
   moveRight();
   }
 
-  else if(leftdistance >= 100 && rightdistance >=100 && frontdistance >= 100){
+  
+    }
+    else if(leftdistance >= 100 && rightdistance >=100 && frontdistance >= 100){
     stopMotors();
     delay(100);
-    }
 }
 
 else 
@@ -79,51 +130,6 @@ else
 moveForward();
 }
 
-  void stopMotors(){
-    digitalWrite(leftmotorPinA,LOW);
-    digitalWrite(leftmotorPinB,LOW);
-    digitalWrite(rightmotorPinA,LOW);
-    digitalWrite(righttmotorPinB,LOW);
-    }
-
-  void moveForward(){
-    digitalWrite(leftmotorPinA,HIGH);
-    digitalWrite(leftmotorPinB,LOW);
-    digitalWrite(rightmotorPinA,HIGH);
-    digitalWrite(righttmotorPinB,LOW);
-  }
-
-  void moveLeft(){
-
-    digitalWrite(leftmotorPinA,LOW);
-    digitalWrite(leftmotorPinB,HIGH);
-    digitalWrite(rightmotorPinA,HIGH);
-    digitalWrite(rightmotorPinB,LOW);
-    delay(500);
-    stopMotors();
-    }
-
-    void moveRight(){
-
-      digitalWrite(leftmotorPinA,HIGH);
-      digitalWrite(leftmotorPinB,LOW);
-      digitalWrite(righttmotorPinA,LOW);
-      digitalWrite(rightmotorPinB,HIGH);
-      delay(500);
-      stopMotors();
-
-      
-      }
-
-      void turn180Degree(){
-
-        digitalWrite(leftmotorPinA,HIGH);
-      digitalWrite(leftmotorPinB,LOW);
-      digitalWrite(righttmotorPinA,LOW);
-      digitalWrite(rightmotorPinB,HIGH);
-      delay(2000);
-      stopMotors();
-        }
+ 
   }
   
-}
